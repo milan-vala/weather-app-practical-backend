183bef0aecb4b34ef895293b3811ca8c29aa8636
milan vala <milanvala31@gmail.com>
2023-10-21 02:39:25 +0530

    fix

--- app.js
+++ app.js
@@ -5,19 +5,16 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
-console.log("token ==>", process.env.TOKEN_SECRET);
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,13 +26,19 @@ function generateToken(username) {
 }
 
 app.post("/api/signup", (req, res) => {
-  const token = generateToken({ username: req.body.username });
-  console.log("token ---->", token);
-  res.json(token);
+  const { username, password, confirmPassword } = req.body;
+  if (
+    !username ||
+    !password ||
+    !confirmPassword ||
+    password !== confirmPassword
+  ) {
+    res.status(400).json({ message: "Bad request." });
+  } else {
+    const token = generateToken({ username: req.body.username });
+    res.json(token);
+  }
 });
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -44,6 +47,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  console.log("error =>", err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
